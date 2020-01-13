18701a13c96cbd05711a488036992bf8b4eba31a
Sandra <32596579+sandra1974@users.noreply.github.com>
2020-01-13 08:33:44 +0100

    Add files via upload

---
+++ Controllers/NotesController.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using NotesApp.DAL;
+using NotesApp.Models;
+using PagedList;
+
+namespace NotesApp.Controllers
+{
+    public class NotesController : Controller
+    {
+        private NoteContext db = new NoteContext();
+
+        // GET: Notes
+
+        [Authorize]
+        public ActionResult Index(string sortOrder, string searchString, string currentFilter, int? page)
+        {
+            ViewBag.CurrentSort = sortOrder;
+            ViewBag.NameSortParm = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            ViewBag.DateSortParm = sortOrder == "Date" ? "date_desc" : "Date";
+
+            if (searchString != null)
+            {
+                page = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+
+            ViewBag.CurrentFilter = searchString;
+
+
+
+            var notes = from n in db.Note
+                           select n;
+
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                notes = notes.Where(n => n.Title.Contains(searchString)
+                                       || n.Description.Contains(searchString));
+            }
+
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    notes = notes.OrderByDescending(n => n.Title);
+                    break;
+                case "Date":
+                    notes = notes.OrderBy(n => n.CreatedOn);
+                    break;
+                case "date_desc":
+                    notes = notes.OrderByDescending(n => n.CreatedOn);
+                    break;
+                default:
+                    notes = notes.OrderBy(n => n.Title);
+                    break;
+            }
+            int pageSize = 5;
+            int pageNumber = (page ?? 1);
+            return View(notes.ToPagedList(pageNumber, pageSize));
+        }
+
+        // GET: Notes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Note note = db.Note.Find(id);
+            if (note == null)
+            {
+                return HttpNotFound();
+            }
+            return View(note);
+        }
+
+        // GET: Notes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Notes/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Title,Description,CreatedOn")] Note note)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Note.Add(note);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(note);
+        }
+
+        // GET: Notes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Note note = db.Note.Find(id);
+            if (note == null)
+            {
+                return HttpNotFound();
+            }
+            return View(note);
+        }
+
+        // POST: Notes/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Title,Description,CreatedOn")] Note note)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(note).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(note);
+        }
+
+        // GET: Notes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Note note = db.Note.Find(id);
+            if (note == null)
+            {
+                return HttpNotFound();
+            }
+            return View(note);
+        }
+
+        // POST: Notes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Note note = db.Note.Find(id);
+            db.Note.Remove(note);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
