b04af71f560dbf0af9319c080ac0100a4654ae95
Sandra <32596579+sandra1974@users.noreply.github.com>
2020-01-13 08:35:23 +0100

    Add files via upload

---
+++ DAL/NoteContext.cs
@@ -0,0 +1,25 @@
+﻿using NotesApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace NotesApp.DAL
+{
+    public class NoteContext : DbContext
+    {
+
+        public NoteContext() : base("NoteContext") { }
+
+        public DbSet<Note> Note { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+
+    }
+
+}
---
+++ DAL/NoteInitializer.cs
@@ -0,0 +1,28 @@
+﻿using NotesApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace NotesApp.DAL
+{
+    public class NoteInitializer : System.Data.Entity.  DropCreateDatabaseIfModelChanges<NoteContext>
+    {
+        protected override void Seed(NoteContext context)
+        {
+            var note = new List<Note> {
+            new Note {Title = "Reminder", Description = "Visit dentist on Monday at 7pm", CreatedOn = DateTime.Parse("2020-01-05")},
+             new Note {Title="Task", Description="Buy present for Xmas", CreatedOn=DateTime.Parse("2019-12-20")},
+            new Note {Title="Meeting", Description="Tomorrow at 7 pm meeting with my friend", CreatedOn=DateTime.Parse("2020-01-12")},
+                new Note {Title="Call", Description="Call client to check if he is satisfied with our product", CreatedOn=DateTime.Parse("2020-01-15")},
+                             };
+          
+            note.ForEach(s => context.Note.Add(s));
+            context.SaveChanges();
+        }
+
+
+
+    }
+}
