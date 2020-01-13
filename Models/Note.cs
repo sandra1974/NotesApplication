dfffda04f3c2fe6ace45f177096e2b62dd98bab9
Sandra <32596579+sandra1974@users.noreply.github.com>
2020-01-13 08:38:28 +0100

    Add files via upload

---
+++ Models/Note.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace NotesApp.Models
+{
+    public class Note
+    {
+    public int ID { get; set; }
+
+
+        [Required]
+        [Display(Name = "Title")]
+        public string Title { get; set; }
+
+
+        [Required]
+        [Display(Name = "Description")]
+        public string Description { get; set; }
+
+       
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString ="{0:yyyy-MM-dd}", ApplyFormatInEditMode=true)]
+      
+        public DateTime CreatedOn { get; set; }
+
+    }
+}
